ec4110c08976c0b056b8060f63388a7d0ea2e51f
Nastia Kuzina <miowch@gmail.com>
2023-05-16 23:24:27 +0200

    Section 3. Coding Challenge 2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,19 +1,9 @@
 'use strict';
 
-// Section 3. Coding Challenge 1
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// Section 3. Coding Challenge 2
 
-const averageDolphins = calcAverage(85, 54, 41);
-const averageKoalas = calcAverage(23, 34, 27);
+const calcTip = (billValue) => billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
 
-function checkWinner(avgDolphins, avgKoalas) {
-    if (avgDolphins >= avgKoalas * 2) {
-        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
-    } else if (avgKoalas >= avgDolphins * 2) {
-        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
-    } else {
-        console.log('No team wins...');
-    }
-}
-
-checkWinner(averageDolphins, averageKoalas);
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
