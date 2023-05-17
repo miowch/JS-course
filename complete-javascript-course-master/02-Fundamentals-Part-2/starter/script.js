bafe012c871424d005b265fe59105dac648d4370
Nastia Kuzina <miowch@gmail.com>
2023-05-18 00:56:06 +0200

    Section 3. Coding Challenge 3

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
+// Section 3. Coding Challenge 1
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const averageDolphins = calcAverage(85, 54, 41);
+const averageKoalas = calcAverage(23, 34, 27);
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= avgDolphins * 2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log('No team wins...');
+    }
+}
+
+checkWinner(averageDolphins, averageKoalas);
+
 // Section 3. Coding Challenge 2
 
 const calcTip = (billValue) => billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
@@ -7,3 +25,33 @@ const calcTip = (billValue) => billValue >= 50 && billValue <= 300 ? billValue *
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// Section 3. Coding Challenge 3
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBmi: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBmi: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+mark.calcBmi();
+john.calcBmi();
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`);
+} else {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`);
+};
